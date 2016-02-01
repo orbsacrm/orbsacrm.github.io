c2699cc4b580f14d7645d5a4778b748509a7c49d
yamadapc <tacla.yamada@gmail.com>
2016-02-01 16:21:58 -0300

    Upgrade everything and remove unnecessary tasks
    Upgrades dev dependencies and removes extraneous tasks; everything
    "image minification" related can be currently pushed to imagemin.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,9 @@
-module.exports = function(grunt) {
+var autoprefixer = require('autoprefixer');
 
+module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
 
-var PathConfig = require('./grunt-settings.js');
+  var PathConfig = require('./grunt-settings.js');
 
   // tasks
   grunt.initConfig({
@@ -14,7 +15,12 @@ var PathConfig = require('./grunt-settings.js');
     clean: {
       options: { force: true },
       temp: {
-        src: ["<%= config.cssDir %>**/*.map", "<%= config.imgDir %>", "<%= config.cssMainFileDir %><%= config.cssMainFileName %>.css.map", "./jpgtmp.jpg"]
+        src: [
+          "<%= config.cssDir %>**/*.map",
+          "<%= config.imgDir %>",
+          "<%= config.cssMainFileDir %><%= config.cssMainFileName %>.css.map",
+          "./jpgtmp.jpg"
+        ]
       }
     },
 
@@ -23,7 +29,7 @@ var PathConfig = require('./grunt-settings.js');
         options: {
           map: true,
           processors: [
-            require('autoprefixer-core')({browsers: ['last 4 version', 'Android 4']})
+            autoprefixer({browsers: ['last 4 version', 'Android 4']})
           ]
         },
         src: ['<%= config.cssDir %>*.css',
@@ -32,22 +38,23 @@ var PathConfig = require('./grunt-settings.js');
               '!<%= config.cssDir %>bootstrap.min.css',
               '!<%= config.cssDir %>ie.css',
               '!<%= config.cssDir %>ie8.css'
-              ]
+             ]
       },
       dist: {
         options: {
           map: false,
           processors: [
-            require('autoprefixer-core')({browsers: ['last 4 version', 'Android 4']})
+            autoprefixer({browsers: ['last 4 version', 'Android 4']})
           ]
         },
-        src: ['<%= config.cssDir %>*.css',
-              '<%= config.cssMainFileDir %><%= config.cssMainFileName %>.css',
-              '!<%= config.cssDir %>bootstrap.css',
-              '!<%= config.cssDir %>bootstrap.min.css',
-              '!<%= config.cssDir %>ie.css',
-              '!<%= config.cssDir %>ie8.css'
-              ]
+        src: [
+          '<%= config.cssDir %>*.css',
+          '<%= config.cssMainFileDir %><%= config.cssMainFileName %>.css',
+          '!<%= config.cssDir %>bootstrap.css',
+          '!<%= config.cssDir %>bootstrap.min.css',
+          '!<%= config.cssDir %>ie.css',
+          '!<%= config.cssDir %>ie8.css'
+        ]
       }
     },
 
@@ -63,11 +70,17 @@ var PathConfig = require('./grunt-settings.js');
           {
             expand: true,
             cwd: '<%= config.sassDir %>',
-            src: ['**/*.scss', '!<%= config.sassMainFileName %>.scss'],
+            src: [
+              '**/*.scss',
+              '!<%= config.sassMainFileName %>.scss'
+            ],
             dest: '<%= config.cssDir %>',
             ext: '.css'
           },
-          {src: '<%= config.sassDir %><%= config.sassMainFileName %>.scss', dest: '<%= config.cssMainFileDir %><%= config.cssMainFileName %>.css'}
+          {
+            src: '<%= config.sassDir %><%= config.sassMainFileName %>.scss',
+            dest: '<%= config.cssMainFileDir %><%= config.cssMainFileName %>.css'
+          }
         ]
       },
       dist: {
@@ -79,11 +92,17 @@ var PathConfig = require('./grunt-settings.js');
           {
             expand: true,
             cwd: '<%= config.sassDir %>',
-            src: ['**/*.scss', '!<%= config.sassMainFileName %>.scss'],
+            src: [
+              '**/*.scss',
+              '!<%= config.sassMainFileName %>.scss'
+            ],
             dest: '<%= config.cssDir %>',
             ext: '.css'
           },
-          {src: '<%= config.sassDir %><%= config.sassMainFileName %>.scss', dest: '<%= config.cssMainFileDir %><%= config.cssMainFileName %>.css'}
+          {
+            src: '<%= config.sassDir %><%= config.sassMainFileName %>.scss',
+            dest: '<%= config.cssMainFileDir %><%= config.cssMainFileName %>.css'
+          }
         ]
       },
       min: {
@@ -99,7 +118,10 @@ var PathConfig = require('./grunt-settings.js');
             dest: '<%= config.cssDir %>',
             ext: '.min.css'
           },
-          {src: '<%= config.sassDir %><%= config.sassMainFileName %>.scss', dest: '<%= config.cssMainFileDir %><%= config.cssMainFileName %>.min.css'}
+          {
+            src: '<%= config.sassDir %><%= config.sassMainFileName %>.scss',
+            dest: '<%= config.cssMainFileDir %><%= config.cssMainFileName %>.min.css'
+          }
         ]
       }
     },
@@ -112,23 +134,16 @@ var PathConfig = require('./grunt-settings.js');
       },
       images: {
         files: ['<%= config.imgSourceDir %>**/*.*'],
-        tasks: [/*'img:jpg', 'newer:pngmin:all', 'newer:svgmin'*/ 'newer:copy:images'],
+        tasks: ['newer:copy:images'],
         options: {
-            spawn: false
-        }
-      },
-      svgSprites: {
-        files: ['<%= config.imgSourceDir %>svg-icons/*.*'],
-        tasks: ['svgstore', 'svg2string'],
-        options: {
-            spawn: false
+          spawn: false
         }
       },
       css: {
         files: ['<%= config.sassDir %>**/*.scss'],
         tasks: ['sass:dev', 'postcss:dev'],
         options: {
-            spawn: false,
+          spawn: false,
         }
       }
     },
@@ -139,7 +154,6 @@ var PathConfig = require('./grunt-settings.js');
         cwd: '<%= config.imgSourceDir %>',
         src: '**',
         dest: '<%= config.imgDir %>',
-        //flatten: true,
         filter: 'isFile',
       }
     },
@@ -149,81 +163,10 @@ var PathConfig = require('./grunt-settings.js');
         files: [{
           expand: true,
           cwd: '<%= config.imgSourceDir %>',
-          src: ['**/*.{jpg,gif}'],
+          src: ['**/*.{jpg,gif,png,svg}'],
           dest: '<%= config.imgDir %>'
         }]
       }
-    },
-
-    svgmin: {
-      options: {
-       plugins: [
-         {
-             removeViewBox: false
-         }, {
-             removeUselessStrokeAndFill: false
-         }
-       ]
-      },
-      dist: {
-       files: [
-          {
-            expand: true,
-            src: ['**/*.svg'],
-            cwd: '<%= config.imgSourceDir %>',
-            dest: '<%= config.imgDir %>'
-          }
-        ]
-      }
-    },
-
-    svgstore: {
-      options: {
-        prefix : 'icon-', // This will prefix each ID
-        svg: { // will add and overide the the default xmlns="http://www.w3.org/2000/svg" attribute to the resulting SVG
-          viewBox : '0 0 100 100',
-          xmlns: 'http://www.w3.org/2000/svg'
-        },
-        cleanup: ['fill']
-      },
-      your_target: {
-        files: {
-          '<%= config.imgDir %>svg-sprites/sprite.svg': ['<%= config.imgDir %>svg-icons/*.svg'],
-        },
-      },
-    },
-
-    svg2string: {
-      elements: {
-        options: {
-          template: '(window.SVG_SPRITES = window.SVG_SPRITES || {})["[%= filename %]"] = [%= content %];',
-          wrapLines: false
-        },
-        files: {
-          '<%= config.jsDir %>svg-sprites.js': [
-            // '<%= config.imgDir %>sprite.svg',
-            '<%= config.imgDir %>svg-sprites/sprite.svg'
-          ]
-        }
-      }
-    },
-
-    // lossy image optimizing (compress png images with pngquant)
-    pngmin: {
-      all: {
-        options: {
-          ext: '.png',
-          force: true
-        },
-        files: [
-          {
-            expand: true,
-            src: ['**/*.png'],
-            cwd: '<%= config.imgSourceDir %>',
-            dest: '<%= config.imgDir %>'
-          }
-        ]
-      },
     },
 
     //Keep multiple browsers & devices in sync when building websites.
@@ -265,13 +208,14 @@ var PathConfig = require('./grunt-settings.js');
               '!<%= config.cssDir %>bootstrap.css',
               '!<%= config.cssDir %>ie.css',
               '!<%= config.cssDir %>ie8.css'
-              ],
+             ],
         ext: '.css'
       },
       dist: {
         expand: true,
         files: {
-          '<%= config.cssMainFileDir %><%= config.cssMainFileName %>.css' : '<%= config.cssMainFileDir %><%= config.cssMainFileName %>.css'
+          '<%= config.cssMainFileDir %><%= config.cssMainFileName %>.css':
+            '<%= config.cssMainFileDir %><%= config.cssMainFileName %>.css'
         },
       }
     },
@@ -292,7 +236,8 @@ var PathConfig = require('./grunt-settings.js');
       },
       dist: {
         files: {
-          '<%= config.cssMainFileDir %><%= config.cssMainFileName %>.css' : '<%= config.cssMainFileDir %><%= config.cssMainFileName %>.css'
+          '<%= config.cssMainFileDir %><%= config.cssMainFileName %>.css':
+            '<%= config.cssMainFileDir %><%= config.cssMainFileName %>.css'
         },
       }
     },
@@ -303,9 +248,9 @@ var PathConfig = require('./grunt-settings.js');
           host: '<%= config.sftpServer %>',
           port: '<%= config.sftpPort %>',
           authKey: {
-                    "username": "<%= config.sftpLogin %>",
-                    "password": "<%= config.sftpPas %>"
-                  }
+            "username": "<%= config.sftpLogin %>",
+            "password": "<%= config.sftpPas %>"
+          }
         },
         cache: 'sftpCache.json',
         src: 'css',
@@ -326,9 +271,6 @@ var PathConfig = require('./grunt-settings.js');
   //watch + browser sync
   grunt.registerTask('dev', ['browserSync', 'watch']);
 
-  //create svg sprite
-  grunt.registerTask('svgsprite', ['svgmin', 'svgstore', 'svg2string']);
-
   grunt.registerTask('default', ['dev']);
 
   // upload to server
@@ -338,7 +280,7 @@ var PathConfig = require('./grunt-settings.js');
   grunt.registerTask('cssbeauty', ['sass:dist', 'cmq:dist', 'postcss:dist', 'csscomb:dist']);
 
   //img minify
-  grunt.registerTask('imgmin', ['imagemin', 'pngmin:all', 'svgmin']);
+  grunt.registerTask('imgmin', ['imagemin']);
 
   //final build
   grunt.registerTask('dist', ['clean:temp', 'imgmin', 'cssbeauty']);
